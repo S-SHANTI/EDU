e928ffc6524cc1507cd8c68e4270045da0c15cdf
Shanti <soheil.amiralaii@gmail.com>
2023-03-22 19:43:56 +0330

    57. تغییر صدای گوشی با استفاده از seekbar

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.testproject
 
-import android.content.Intent
+import android.media.AudioManager
 import android.os.Bundle
-import android.widget.ProgressBar
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
-import java.time.Duration
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,17 +17,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                binding.seekBar.progress=progress
+        val audiomanager = getSystemService(AUDIO_SERVICE) as AudioManager
+
+        binding.seekBar.max = audiomanager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        binding.seekBar.progress = audiomanager.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                audiomanager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                Toast.makeText(this@MainActivity,seekBar?.progress.toString(),Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    seekBar?.progress.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                Toast.makeText(this@MainActivity,seekBar?.progress.toString(),Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    seekBar?.progress.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
         })
