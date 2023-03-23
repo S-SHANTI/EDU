ef5b1272d0a4c0f524a5f68b263175854c0435ea
Shanti <soheil.amiralaii@gmail.com>
2023-03-24 00:45:13 +0330

    58. بخش اول ایجاد امتیازدهی با استفاده از RATINGBAR

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.testproject
 
-import android.media.AudioManager
 import android.os.Bundle
-import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
@@ -17,37 +15,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val audiomanager = getSystemService(AUDIO_SERVICE) as AudioManager
+        val shared = getSharedPreferences("test", MODE_PRIVATE )
 
-        binding.seekBar.max = audiomanager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-        binding.seekBar.progress = audiomanager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        binding.ratingBar.rating = shared.getFloat("rate",3f)
 
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(
-                seekBar: SeekBar?,
-                progress: Int,
-                fromUser: Boolean
-            ) {
-                audiomanager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
-            }
+        binding.ratingBar.setOnRatingBarChangeListener { _, rating, _  ->
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                Toast.makeText(
-                    this@MainActivity,
-                    seekBar?.progress.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+            val edit = shared.edit()
+            edit.putFloat("rate", rating)
+            edit.apply()
+            Toast.makeText(this,rating.toString(),Toast.LENGTH_SHORT).show()
 
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                Toast.makeText(
-                    this@MainActivity,
-                    seekBar?.progress.toString(),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-        })
+        }
     }
 
 }
