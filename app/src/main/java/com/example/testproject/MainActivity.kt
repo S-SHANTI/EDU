ab86dac877c510db147190b79f6551a2dffd0e18
Shanti <soheil.amiralaii@gmail.com>
2023-03-24 02:47:15 +0330

    60. طریقه استفاده بهینه از view

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.testproject
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
 
@@ -15,18 +14,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val shared = getSharedPreferences("test", MODE_PRIVATE )
-
-        binding.ratingBar.rating = shared.getFloat("rate",3f)
-
-        binding.ratingBar.setOnRatingBarChangeListener { _, rating, _  ->
-
-            val edit = shared.edit()
-            edit.putFloat("rate", rating)
-            edit.apply()
-            Toast.makeText(this,rating.toString(),Toast.LENGTH_SHORT).show()
-
-        }
     }
 
 }
