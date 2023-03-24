5cb8b924cda2d377e2194c00a1c177dcfbcee906
Shanti <soheil.amiralaii@gmail.com>
2023-03-24 21:29:11 +0330

    ۶۷. شیوه تغییر دکمه بازگشت با on back pressesd

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.testproject
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private var onDoubleClick = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +20,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        toast("onStart")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        toast("onResume")
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (onDoubleClick) {
+            super.onBackPressed()
+            return
+        }
+        onDoubleClick = true
+        toast("لطفن دوباره کلیک کنید")
+        Handler(Looper.getMainLooper()).postDelayed({onDoubleClick=false},3000)
     }
-
     private fun toast(text : String){
         Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
     }
