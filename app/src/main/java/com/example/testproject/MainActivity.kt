110b376498ae7cf675cfdd9f8a085a6d97ade907
Shanti <soheil.amiralaii@gmail.com>
2023-03-24 20:43:16 +0330

    ۶۶. چرخه حیات اکتیویتی

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.testproject
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
@@ -16,49 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var isAllfabschecked = false
-        binding.extendFabShow.shrink()
-
-        binding.extendFabShow.setOnClickListener {
-
-            isAllfabschecked = if (isAllfabschecked) {
-                binding.extendFabShow.extend()
-                hide()
-                false
-            } else {
-                binding.extendFabShow.shrink()
-                show()
-                true
-            }
-        }
-
     }
 
-    private fun show() {
-        binding.fabEdit.show()
-        binding.fabDelete.show()
-        binding.fabShare.show()
-        binding.extendFabShow.extend()
-
+    override fun onStart() {
+        super.onStart()
+        toast("onStart")
     }
 
-    private fun hide() {
-        binding.fabEdit.hide()
-        binding.fabDelete.hide()
-        binding.fabShare.hide()
-        binding.extendFabShow.shrink()
+    override fun onResume() {
+        super.onResume()
+        toast("onResume")
     }
-    fun onClick(view : View) {
 
-        when (view.id) {
-
-            R.id.fab_edit -> toast("ویرایش کردن")
-            R.id.fab_delete -> toast("پاک کردن")
-            R.id.fab_share -> toast("استراک گذاری")
-        }
-    }
-    private fun toast(text: String){
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    private fun toast(text : String){
+        Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
     }
 }
 
