77e5fba1fb46c87791c68c6aba1123335ccf155e
Shanti <soheil.amiralaii@gmail.com>
2023-03-24 17:16:43 +0330

    63.پیاده‌سازی یک مینی پروژه با استفاده از floating Action Button

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.testproject
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
 
@@ -14,17 +16,54 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val ischecked = false
+        var isAllfabschecked = false
+        binding.extendFabShow.shrink()
 
-        binding.fabShow.setOnClickListener {
+        binding.extendFabShow.setOnClickListener {
 
-            if (ischecked) {
-                binding.fabShow.extend()
+            isAllfabschecked = if (isAllfabschecked) {
+                binding.extendFabShow.extend()
+                hide()
+                false
             } else {
-                binding.fabShow.shrink()
+                binding.extendFabShow.shrink()
+                show()
+                true
             }
         }
 
     }
 
+    private fun show() {
+        binding.fabEdit.show()
+        binding.fabDelete.show()
+        binding.fabShare.show()
+        binding.extendFabShow.extend()
+
+    }
+
+    private fun hide() {
+        binding.fabEdit.hide()
+        binding.fabDelete.hide()
+        binding.fabShare.hide()
+        binding.extendFabShow.shrink()
+    }
+    fun onClick(view : View) {
+
+        when (view.id) {
+
+            R.id.fab_edit -> toast("ویرایش کردن")
+            R.id.fab_delete -> toast("پاک کردن")
+            R.id.fab_share -> toast("استراک گذاری")
+        }
+    }
+    private fun toast(text: String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
 }
+
+
+
+
+
+
