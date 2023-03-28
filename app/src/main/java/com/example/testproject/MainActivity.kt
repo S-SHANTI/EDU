b1141e3983125207a48009fe5ed2558b63ed3a9c
Shanti <soheil.amiralaii@gmail.com>
2023-03-28 15:39:45 +0330

    ۷۸. ایجاد حالت افقی برای view  ها

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,39 +1,27 @@
 package com.example.testproject
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
 
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var onDoubleClick = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//برای تغییر اسمم پروزه در خروجی از خط کد زیر استفاده می‌شود، همچنین در قسمت manifest ‌نیز می‌توان انجام داد
+        title="TEST"
 
-    }
-
-
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (onDoubleClick) {
-            super.onBackPressed()
-            return
+        binding.button.setOnClickListener {
+            binding.textView.text = "TEST"
         }
-        onDoubleClick = true
-        toast("لطفن دوباره کلیک کنید")
-        Handler(Looper.getMainLooper()).postDelayed({onDoubleClick=false},3000)
     }
-    private fun toast(text : String){
-        Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
-    }
+
+
 }
 
 
