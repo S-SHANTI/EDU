805e80f8bf958c4c83aa077d876be43b0b12ad41
Shanti <soheil.amiralaii@gmail.com>
2023-03-29 03:56:10 +0330

    ۸۰. بخش اول آموزش کار با media player

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.testproject
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject.databinding.ActivityMainBinding
@@ -7,21 +8,32 @@ import com.example.testproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private var mediaplayer : MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//برای تغییر اسمم پروزه در خروجی از خط کد زیر استفاده می‌شود، همچنین در قسمت manifest ‌نیز می‌توان انجام داد
-        title="TEST"
 
-        binding.button.setOnClickListener {
-            binding.textView.text = "TEST"
-        }
+        binding.fabStarMusic.setOnClickListener { onStartMusic() }
+        binding.fabStarMusic.setOnClickListener { onpPuseMusic() }
+        binding.fabStarMusic.setOnClickListener { onEndtMusic() }
     }
 
+    private fun onStartMusic() {
+       if (mediaplayer == null)
+           mediaplayer = MediaPlayer.create(this,R.raw.music)
+    }
 
+    private fun onpPuseMusic() {
+    mediaplayer?.pause()
+    }
+
+    private fun onEndtMusic() {
+        mediaplayer?.stop()
+        mediaplayer?.release()
+        mediaplayer = null
+    }
 }
 
 
